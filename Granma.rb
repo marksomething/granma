3b2314a1d6b13b25056b7ea4a7bdb1cd40747909
mark <mark@marksomething.com>
2013-05-09 23:24:07 +1000

    add SQL for Compare subclass

--- Granma.rb
+++ Granma.rb
@@ -65,23 +65,28 @@ module Granma
 			},
 			{
 				:phrases => ["at most","less than or equal to"],
-				:lambda => lambda {|left,right| left <= right }
+				:lambda => lambda {|left,right| left <= right },
+				:sql => " <= "
 			},
 			{
 				:phrases => ["more than","greater than"],
-				:lambda => lambda {|left,right| left > right }
+				:lambda => lambda {|left,right| left > right },
+				:sql => " > "
 			},
 			{
 				:phrases => ["less than"],
-				:lambda => lambda {|left,right| left < right }
+				:lambda => lambda {|left,right| left < right },
+				:sql => " < "
 			},
 			{
 				:phrases => ["equal to","exactly"],
-				:lambda => lambda {|left,right| left == right }
+				:lambda => lambda {|left,right| left == right },
+				:sql => " = "
 			},
 			{
 				:phrases => ["not equal to"],
-				:lambda => lambda {|left,right| left != right }
+				:lambda => lambda {|left,right| left != right },
+				:sql => " <> "
 			}
 		]
 	end
